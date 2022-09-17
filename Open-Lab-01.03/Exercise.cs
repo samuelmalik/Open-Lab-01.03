db7bbeb7811308a2d13437dbce77f7ac19ce5eee
samuelmalik <113421307+samuelmalik@users.noreply.github.com>
2022-09-17 16:37:47 +0200

    Update Exercise.cs

--- Open-Lab-01.03/Exercise.cs
+++ Open-Lab-01.03/Exercise.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_01._03
 {
@@ -6,6 +6,7 @@ namespace Open_Lab_01._03
     {
         public int Addition(int number)
         {
+            return number + 1;
             throw new NotImplementedException();
         }
     }
